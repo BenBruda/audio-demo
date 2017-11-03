aff7acb79b7f0a738dbe482dfaaad8463c3be92b
Ben Brud <ben.bruda@gmail.com>
2017-11-03 16:00:44 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+  super()
+  this.state={recording:false}
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  this.audioContext = new AudioContext();
+  }
+
+  componentDidMount(){
+    if (navigator.mediaDevices) {
+      console.log('getUserMedia supported.');
+      navigator.getUserMedia({audio: true}, (stream)=> {
+        this.audio.srcObject = stream;
+        this.audio.play()
+      },(err) =>{
+          console.log('err' + err);
+      });
+  
+    } else {
+    console.log('getUserMedia not supported!');
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        start audio 
+        <audio  muted={!this.state.recording} ref={(x) => this.audio = x} />
+        <input type='checkbox' value={this.state.recording} onChange={(e) =>this.setState({recording:e.target.checked})} />
+      </div>
+     
+    );
+  }
+}
+
+
+
+export default App;
